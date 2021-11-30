a1bf81b0eb93588d41f9ad54d92ead163a5fad94
SimonKozyirev <skozyirev@gmail.com>
2021-11-30 09:57:34 +0300

    Map

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainActivity.kt
@@ -2,11 +2,138 @@ package com.makentoshe.androidgithubcitemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.DisplayMetrics
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.MinimapOverlay
+import org.osmdroid.views.overlay.ScaleBarOverlay
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
+
+    private lateinit var map : MapView;
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+
+        //handle permissions first, before map is created. not depicted here
+
+        //load/initialize the osmdroid configuration, this can be done
+        // This won't work unless you have imported this: org.osmdroid.config.Configuration.*
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        //setting this before the layout is inflated is a good idea
+        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
+        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
+        //see also StorageUtils
+        //tile servers will get you banned based on this string.
+
+        //inflate and create the map
+        setContentView(R.layout.activity_main);
+
+        map = findViewById<MapView>(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        val mapController = map.controller
+        mapController.setZoom(10.5)
+        val startPoint = GeoPoint(59.9333, 30.3);
+        mapController.setCenter(startPoint);
+        val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), map);
+        locationOverlay.enableMyLocation();
+        map.overlays.add(locationOverlay)
+        val overlay = LatLonGridlineOverlay2()
+        map.overlays.add(overlay)
+        val rotationGestureOverlay = RotationGestureOverlay(this, map);
+        rotationGestureOverlay.isEnabled
+        map.setMultiTouchControls(true);
+        map.overlays.add(rotationGestureOverlay);
+        val compassOverlay = CompassOverlay(this, InternalCompassOrientationProvider(this), map)
+        compassOverlay.enableCompass()
+        map.overlays.add(compassOverlay)
+        val dm : DisplayMetrics = this.resources.displayMetrics
+        val scaleBarOverlay = ScaleBarOverlay(map)
+        scaleBarOverlay.setCentred(true)
+//play around with these values to get the location on screen in the right place for your application
+        scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        map.overlays.add(scaleBarOverlay);
+        val minimapOverlay = MinimapOverlay(this, map.tileRequestCompleteHandler);
+        minimapOverlay.setWidth(dm.widthPixels / 5);
+        minimapOverlay.setHeight(dm.heightPixels / 5);
+//optionally, you can set the minimap to a different tile source
+//minimapOverlay.setTileSource(....);
+        map.overlays.add(minimapOverlay);
+        val marker = Marker(map)
+        marker.position = GeoPoint(59.9333, 30.3)
+        marker.icon = ContextCompat.getDrawable(this, R.drawable.maker_icon)
+        marker.title = "Test Marker"
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+        map.overlays.add(marker)
+        map.invalidate()
+
     }
+
+
+    override fun onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume();//needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    override fun onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionsToRequest = ArrayList<String>();
+        var i = 0;
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i]);
+            i++;
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+
+
+    /*private fun requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+        if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            permissionsToRequest.add(permission);
+        }
+    }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }*/
 }
