a24bbda73bfbc32df93c3a04f73762a3eafc9097
PhAn1om13 <sosnonikita@gmail.com>
2021-12-03 23:18:35 +0300

    MyToastButton

--- app/src/main/java/com/makentoshe/androidgithubcitemplate/MainScreeen.kt
+++ app/src/main/java/com/makentoshe/androidgithubcitemplate/MainScreeen.kt
@@ -18,7 +18,7 @@ class MainScreeen : AppCompatActivity() {
     private lateinit var button : Button
     private lateinit var imageButton: ImageButton
     @RequiresApi(Build.VERSION_CODES.HONEYCOMB)
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //        button = findViewById(R.id.button)
@@ -45,10 +45,54 @@ class MainScreeen : AppCompatActivity() {
             toast.show()
         }
 
+        val textbutton3: TextView = findViewById(R.id.review_id)
+        textbutton3.setOnClickListener {
+            val text_toast3 = "Переход на экран советы и отзывы, скоро..."
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text_toast3, duration)
+            toast.show()
+        }
 
+        val textbutton4: TextView = findViewById(R.id.hike_id)
+        textbutton4.setOnClickListener {
+            val text_toast4 = "Переход на экран подготовка к походу, скоро..."
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text_toast4, duration)
+            toast.show()
+        }
 
+        val textbutton5: TextView = findViewById(R.id.eat_id)
+        textbutton5.setOnClickListener {
+            val text_toast5 = "Переход на экран питание, скоро..."
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text_toast5, duration)
+            toast.show()
+        }
+
+        val ButtonStart: Button = findViewById(R.id.button)
+        ButtonStart.setOnClickListener {
+            val Start = "Не переживайте, скоро начнем :)"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, Start, duration)
+            toast.show()
+        }
+
+        val BackImageButton: ImageButton = findViewById(R.id.back_view_icon_id)
+        BackImageButton.setOnClickListener {
+            val Back = "Подождите, назад вернуться всегда успеете"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, Back, duration)
+            toast.show()
+        }
+
+        val SettingImageButton: ImageButton = findViewById(R.id.setting_icon_id)
+        SettingImageButton.setOnClickListener {
+            val Setting = "Для чего тебе настройки, если приложение еще не готово?"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, Setting, duration)
+            toast.show()
+        }
     }
-
 //
 //    fun New_the_note (view: View) {
 //        Log.d("My Tag", "NewTheNote complete")
